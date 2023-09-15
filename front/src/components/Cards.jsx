62dd12e0ae51f6ed8d7770780a854c68421b46c4
David Henao <davidhenao3105@gmail.com>
2023-09-15 11:01:37 -0500

    change origin

--- front/src/components/Cards.jsx
+++ front/src/components/Cards.jsx
@@ -10,7 +10,7 @@ const Cards = ({ characters }) => {
                status={status}
                species={species}
                gender={gender}
-               origin={origin}
+               origin={origin.name}
                image={image}
             />
          })}
