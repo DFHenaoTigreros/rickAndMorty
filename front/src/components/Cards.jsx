87ebd67be35b0ea20971f22d3a5d6ab8d1f88993
David Henao <davidhenao3105@gmail.com>
2023-09-15 08:42:58 -0500

    [ADD] initial config and components

---
+++ front/src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from './Card';
+
+const Cards = ({ characters }) => {
+   return (
+      <div>
+         {characters.map(({id, name, status, species, gender, origin, image}) => {
+            return <Card
+               key={id}
+               name={name}
+               status={status}
+               species={species}
+               gender={gender}
+               origin={origin}
+               image={image}
+            />
+         })}
+      </div>
+   )
+}
+export default Cards;
