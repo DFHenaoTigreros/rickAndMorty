09962047bb7028eca24497844116927e9d4dc8a0
David Henao <davidhenao3105@gmail.com>
2024-06-24 16:45:25 -0500

    UPDATED TO ENGLISH

--- back/test/index.test.js
+++ back/test/index.test.js
@@ -2,14 +2,14 @@ const {server} = require('../src/app');
 const session = require('supertest');
 const request = session(server);
 
-describe("Test de RUTAS", () => {
+describe("Route Tests", () => {
   describe("GET /rickandmorty/character/:id", () => {
-    it("Responde con status: 200", async () => {
+    it("Responds with status: 200", async () => {
       const response = await request.get('/rickandmorty/character/1');
       expect(response.statusCode).toBe(200);
     });
 
-    it(`"Responde un objeto con las propiedades: "id", "name", "species", "gender", "status", "origin" e "image"`, async () => {
+    it(`"Responds with an object with the properties: "id", "name", "species", "gender", "status", "origin" e "image"`, async () => {
       const response = await request.get('/rickandmorty/character/1');
       const obj = ["id", "name", "species", "gender", "status", "origin", "image"];
       obj.forEach((prop) => {
@@ -17,26 +17,26 @@ describe("Test de RUTAS", () => {
       })
     });
 
-    it("Si hay un error responde con status: 404", async () => {
+    it("If there is an error, responds with status: 404", async () => {
       const response = await request.get('/rickandmorty/character/900');
       expect(response.statusCode).toBe(404);
     });
   });
 
-  describe("_GET /rickandmorty/login_", () => {
-    it("Responde true la propiedad access, si se pasa la información correcta", async () => {
+  describe("GET /rickandmorty/login_", () => {
+    it("Responds with the access property as true, if the correct information is passed", async () => {
       const response = await request.get("/rickandmorty/login?email=davidhenao3105@gmail.com&password=dafeheti3");
       expect(response.body).toEqual({access: true});
     });
 
-    it("Responde false la propiedad access, si se pasa la información incorrecta", async () => {
+    it("Responds with 'incorrect password' if incorrect information is passed", async () => {
       const response = await request.get("/rickandmorty/login?email=davidhenao3105@gmail.com&password=dafeheti3.");
       expect(response.text).toBe("incorrect password");
     });
   });
 
-  describe("_POST /rickandmorty/fav_", () => {
-    it("Responde con un arreglo, lo que envíes por body", async () => {
+  describe("POST /rickandmorty/fav_", () => {
+    it("Responds with an array containing what is sent in the body", async () => {
       const character = {
         id: 1,
         name: "Rick Sanchez",
@@ -47,7 +47,7 @@ describe("Test de RUTAS", () => {
       expect(response.body).toContainEqual(character);
     });
 
-    it("Responde con un arreglo de mas de un elemento, lo que envíes por body", async () => {
+    it("Responds with an array of more than one element, containing what is sent in the body", async () => {
       const character = {
         id: 2,
         name: "Morty Smith",
@@ -58,8 +58,8 @@ describe("Test de RUTAS", () => {
     });
   });
 
-  describe("_DELETE /rickandmorty/fav/:id_", () => {
-    it("", async () => {
+  describe("DELETE /rickandmorty/fav/:id_", () => {
+    it("Responds with an array of more than zero elements after deletion", async () => {
       const response = await request.delete("/rickandmorty/fav/1");
       expect(response.body.length).toBeGreaterThan(0);
     });
